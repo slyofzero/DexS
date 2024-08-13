12aa2ace93149f1cb9e88f1e45fa90f8b765a13e
slyofzero <sly.of.zero@gmail.com>
2024-08-13 14:45:19 +0530

    API

--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,49 @@ import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 puppeteer.use(StealthPlugin());
 
-const DATA_URL =
-  "https://www.geckoterminal.com/solana/pools?sort=-1h_trend_score&networks=solana";
+// const DATA_URL =
+//   "https://www.geckoterminal.com/solana/pools?sort=-1h_trend_score&networks=solana";
 
-async function getTrendingTokens() {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto(DATA_URL || "", {
-    waitUntil: "networkidle0",
-  });
+// async function getTrendingTokens() {
+//   const browser = await puppeteer.launch({ headless: true });
+//   const page = await browser.newPage();
+//   await page.goto(DATA_URL || "", {
+//     waitUntil: "networkidle0",
+//   });
 
-  const trendingPools: string[] = [];
+//   const trendingPools: string[] = [];
 
-  // Wait for the <tbody> element to appear
-  const divSelector = "div.w-full table";
-  await page.waitForSelector(divSelector);
-  const div = (await page.$$(divSelector)).at(2);
-  const rows = (await div?.$$("tbody tr")) || [];
+//   // Wait for the <tbody> element to appear
+//   const divSelector = "div.w-full table";
+//   await page.waitForSelector(divSelector);
+//   const div = (await page.$$(divSelector)).at(2);
+//   const rows = (await div?.$$("tbody tr")) || [];
 
-  for (const row of rows) {
-    const cell = await row.$("td a");
-    const link = await (await cell?.getProperty("href"))?.jsonValue();
-    const pool = link?.split("/").at(-1);
-    if (pool) trendingPools.push(pool);
-  }
+//   for (const row of rows) {
+//     const cell = await row.$("td a");
+//     const link = await (await cell?.getProperty("href"))?.jsonValue();
+//     const pool = link?.split("/").at(-1);
+//     if (pool) trendingPools.push(pool);
+//   }
 
-  await browser.close();
+//   await browser.close();
 
-  return trendingPools;
-}
+//   return trendingPools;
+// }
 
 (async function () {
-  const trendingTokensList = await getTrendingTokens();
-  console.log(trendingTokensList, trendingTokensList.length);
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  // const ws = new WebSocket(DEX_URL || "", { headers: wssHeaders });
+  await page.goto(
+    "https://app.geckoterminal.com/api/p1/solana/pools?include=dex,dex.network,dex.network.network_metric,tokens&page=1&include_network_metrics=true&sort=-1h_trend_score&networks=solana"
+  );
+  const allText = await page.evaluate(() => {
+    return document.body.innerText;
+  });
+  await browser.close();
+
+  console.log(JSON.parse(allText));
+  // const trendingTokensList = await getTrendingTokens();
+  // console.log(trendingTokensList, trendingTokensList.length);
 })();
